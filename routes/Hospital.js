eabc5a90d0903992087e991780049e8418d1c176
abdulkareemm <eng.kreem.96@gmail.com>
2024-07-27 11:44:36 +0300

    add is_auth middleware

--- routes/Hospital.js
+++ routes/Hospital.js
@@ -1,5 +1,6 @@
 const { Create, Get, CreateClinic, Delete_Clinic, Login } = require("../controllers/Hospital");
 const { uploadImage } = require("../middleware/multer");
+const is_auth = require("../middleware/is_auth");
 const router = require("express").Router();
 
 
@@ -8,7 +9,7 @@ router.post("/login", Login);
 router.post(
   "/add-clinic",
   uploadImage.single("logo"),
-//   is_auth,
+  is_auth,
 //   isAdmin,
 //   clinicInfoValidator,
 //   validate,
