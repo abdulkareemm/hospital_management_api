1b3369591c56ee199de2ec18c5ac8c9ee7b547a8
abdulkareemm <eng.kreem.96@gmail.com>
2024-08-01 18:29:50 +0300

    add validator function for create clinic

---
+++ middleware/validator.js
@@ -0,0 +1,57 @@
+const { check, validationResult } = require("express-validator");
+
+exports.clinicInfoValidator = [
+  check("name").trim().not().isEmpty().withMessage("clinic name is missing!"),
+  check("startFrom")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("start from is a required field!"),
+  check("endAt")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("end at is a required field!"),
+  check("type")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("specialist is a required field!!"),
+  check("visitDuration")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("visit duration  is a required field!!"),
+  check("fees")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("fees  is a required field!!"),
+  check("color_highlight")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("color_highlight is a required field!!"),
+  check("mobile")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("mobile is a required field!"),
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password is missing!")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters long!"),
+  check("email").isEmail().withMessage("Email is invalid!"),
+];
+
+exports.validate = (req, res, next) => {
+  const error = validationResult(req).array();
+  if (error.length) {
+    return res.json({ error: error[0].msg });
+  }
+
+  next();
+};
--- routes/Hospital.js
+++ routes/Hospital.js
@@ -1,31 +1,31 @@
-const { Create, Get, Create_Clinic, Delete_Clinic, Login } = require("../controllers/Hospital");
+const {
+  Create,
+  Get,
+  Create_Clinic,
+  Delete_Clinic,
+  Login,
+} = require("../controllers/Hospital");
 const { uploadImage } = require("../middleware/multer");
 const is_auth = require("../middleware/is_auth");
 const { isAdmin } = require("../middleware/is_admin");
+const { clinicInfoValidator, validate } = require("../middleware/validator");
+
 const router = require("express").Router();
 
-
-router.post("/create",uploadImage.single("logo"), Create);
+router.post("/create", uploadImage.single("logo"), Create);
 router.post("/login", Login);
 router.post(
   "/add-clinic",
   uploadImage.single("logo"),
   is_auth,
   isAdmin,
-//   clinicInfoValidator,
-//   validate,
+  clinicInfoValidator,
+  validate,
   Create_Clinic
 );
 
-router.delete("/delete-clinic", 
-is_auth,
- isAdmin, 
- Delete_Clinic);
+router.delete("/delete-clinic", is_auth, isAdmin, Delete_Clinic);
 
 router.get("/", Get);
-
-
-
-
 
 module.exports = router;
