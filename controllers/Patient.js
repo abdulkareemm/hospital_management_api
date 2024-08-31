1916e96a4b528b04f9b799a185a5918193cab219
abdulkareemm <eng.kreem.96@gmail.com>
2024-08-31 13:19:08 +0300

    add check date reservation

--- controllers/Patient.js
+++ controllers/Patient.js
@@ -67,3 +67,178 @@ exports.login = async (req, res) => {
     res.status(500).json({ err: "something wrong!" });
   }
 };
+
+exports.CheckDateReservation = async (req, res) => {
+  try {
+    let { clinicId, time, date, userId } = req.body;
+    date = moment.utc(date, "DD-MM-YYYY").toISOString();
+    //// first get all appointment in clinic in the day who is input by user
+    const appointment = await Clinic.findById(clinicId).populate({
+      path: "appointments",
+      match: { date: { $eq: date } },
+    });
+    const endTime = moment
+      .utc(
+        moment
+          .utc(req.body.time, "HH:mm")
+          .add(appointment.visitDuration, "minutes")
+      )
+      .format("HH:mm");
+
+    //// if there are appointments in this day
+    if (appointment.appointments.length > 0) {
+      const day = moment(date).day();
+      //// if day is not off day
+      if (appointment.days.includes(day)) {
+        //// if time is not in work time
+        if (endTime > appointment.endAt || time < appointment.startFrom) {
+          return res.status(401).json({ msg: "Time is incorrect" });
+        } else {
+          appointment.appointments = appointment.appointments.filter(
+            (element) => {
+              const endTime = moment
+                .utc(
+                  moment
+                    .utc(element.time, "HH:mm")
+                    .add(appointment.visitDuration, "minutes")
+                )
+                .format("HH:mm");
+              if (element.time <= time && time <= endTime) return true;
+              return false;
+            }
+          );
+          //// if there is appointments duration the time input
+          if (appointment.appointments.length > 0) {
+            return res
+              .status(401)
+              .json({ msg: "This time was taken by another user" });
+          } else {
+            ///// get all appointments user to check if he has appointment in other clinic in the same time
+            ///populate with clinic to return name off clinic he has appointment in it
+            const anotherAppointment = await Appointment.find({
+              patient: { $eq: userId },
+              date: { $eq: date },
+            }).populate("clinic");
+
+            if (anotherAppointment.length > 0) {
+              const prev = anotherAppointment.filter((element) => {
+                const end = moment
+                  .utc(
+                    moment
+                      .utc(element.time, "HH:mm")
+                      .add(element.clinic.visitDuration, "minutes")
+                  )
+                  .format("HH:mm");
+
+                if (
+                  (element.time <= time && time <= end) ||
+                  (element.time >= endTime && endTime <= end)
+                )
+                  return true;
+                return false;
+              });
+              if (prev.length > 0) {
+                return res.status(401).json({
+                  msg: `you have another appointment in ${prev[0].clinic.name} duration visit time`,
+                });
+              }
+              const prevAppointment = await Appointment.find({
+                patient: { $eq: userId },
+                clinic: { $eq: clinicId },
+                date: { $gt: moment(date).add("-16", "days").toISOString() },
+              });
+
+              return res.status(201).json({
+                msg: "accepted time you can reverse!",
+                success: true,
+                type: prevAppointment.length > 0 ? true : false,
+              });
+            } else {
+              const prevAppointment = await Appointment.find({
+                patient: { $eq: userId },
+                clinic: { $eq: clinicId },
+                date: { $gt: moment(date).add("-16", "days").toISOString() },
+              });
+
+              return res.status(201).json({
+                msg: "accepted time you can reverse!",
+                success: true,
+                type: prevAppointment.length > 0 ? true : false,
+              });
+            }
+          }
+        }
+      }
+      //// day is off
+      else {
+        return res.status(401).json({ msg: "Day is off, try another day!" });
+      }
+    }
+    //// if there are not  appointments in this day
+    else {
+      const day = moment(date).day();
+      if (appointment.days.includes(day)) {
+        if (endTime > appointment.endAt || time < appointment.startFrom) {
+          return res.status(401).json({ msg: "Time is incorrect" });
+        } else {
+          ///// get all appointments user to check if he has appointment in other clinic in the same time
+          ///populate with clinic to return name off clinic he has appointment in it
+          const anotherAppointment = await Appointment.find({
+            patient: { $eq: userId },
+            date: { $eq: date },
+          }).populate("clinic");
+
+          if (anotherAppointment.length > 0) {
+            const prev = anotherAppointment.filter((element) => {
+              const end = moment
+                .utc(
+                  moment
+                    .utc(element.time, "HH:mm")
+                    .add(element.clinic.visitDuration, "minutes")
+                )
+                .format("HH:mm");
+
+              if (
+                (element.time <= time && time <= end) ||
+                (element.time >= endTime && endTime <= end)
+              )
+                return true;
+              return false;
+            });
+            if (prev.length > 0) {
+              return res.status(401).json({
+                msg: `you have another appointment in ${prev[0].clinic.name} duration visit time`,
+              });
+            }
+            const prevAppointment = await Appointment.find({
+              patient: { $eq: userId },
+              clinic: { $eq: clinicId },
+              date: { $gt: moment(date).add("-16", "days").toISOString() },
+            });
+            return res.status(201).json({
+              msg: "accepted time you can reverse!",
+              success: true,
+              type: prevAppointment.length > 0 ? true : false,
+            });
+          } else {
+            const prevAppointment = await Appointment.find({
+              patient: { $eq: userId },
+              clinic: { $eq: clinicId },
+              date: { $gt: moment(date).add("-16", "days").toISOString() },
+            });
+            return res.status(201).json({
+              msg: "accepted time you can reverse!",
+              success: true,
+              type: prevAppointment.length > 0 ? true : false,
+            });
+          }
+        }
+      } else {
+        return res.status(401).json({ msg: "Day is off, try another day!" });
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: "something wrong!" });
+  }
+};
